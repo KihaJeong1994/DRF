b1a3d2401004f3fe16ac02a4c8eb96c8dc813056
KihaJeong <jkha7371@gmail.com>
2021-08-18 17:36:57 +0900

    react django docker

---
+++ blog_api/serializers.py
@@ -0,0 +1,11 @@
+from typing import ClassVar
+
+from django.db import models
+from rest_framework import serializers
+from blog.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'author', 'excerpt', 'content', 'status',)
