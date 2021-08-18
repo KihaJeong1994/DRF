781736a6a99cb7b06042c04de4cb3da4baf7f2ce
kihamac <jkha7371@gmail.com>
2021-08-18 23:04:52 +0900

    test and insert

--- blog_api/views.py
+++ blog_api/views.py
@@ -6,8 +6,75 @@ from .serializers import PostSerializer
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
-    pass
+
 
 
 class PostDetail(generics.RetrieveDestroyAPIView):
-    pass
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+'''
+
+CreateAPIView
+Used for create-only endpoints.
+
+Provides a post method handler.
+
+Extends: GenericAPIView, CreateModelMixin
+
+ListAPIView
+Used for read-only endpoints to represent a collection of model instances.
+
+Provides a get method handler.
+
+Extends: GenericAPIView, ListModelMixin
+
+RetrieveAPIView
+Used for read-only endpoints to represent a single model instance.
+
+Provides a get method handler.
+
+Extends: GenericAPIView, RetrieveModelMixin
+
+DestroyAPIView
+Used for delete-only endpoints for a single model instance.
+
+Provides a delete method handler.
+
+Extends: GenericAPIView, DestroyModelMixin
+
+UpdateAPIView
+Used for update-only endpoints for a single model instance.
+
+Provides put and patch method handlers.
+
+Extends: GenericAPIView, UpdateModelMixin
+
+ListCreateAPIView
+Used for read-write endpoints to represent a collection of model instances.
+
+Provides get and post method handlers.
+
+Extends: GenericAPIView, ListModelMixin, CreateModelMixin
+
+RetrieveUpdateAPIView
+Used for read or update endpoints to represent a single model instance.
+
+Provides get, put and patch method handlers.
+
+Extends: GenericAPIView, RetrieveModelMixin, UpdateModelMixin
+
+RetrieveDestroyAPIView
+Used for read or delete endpoints to represent a single model instance.
+
+Provides get and delete method handlers.
+
+Extends: GenericAPIView, RetrieveModelMixin, DestroyModelMixin
+
+RetrieveUpdateDestroyAPIView
+Used for read-write-delete endpoints to represent a single model instance.
+
+Provides get, put, patch and delete method handlers.
+
+Extends: GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+'''
